09f92835ce091f8c7dd147c6cf562a798a11c5e8
jamesmillar <James@sailr.co.uk>
2023-03-01 13:32:48 +0000

    Update plugin.xml

--- cordova-plugin-wallet-suppression/src/android/WalletSuppression.java
+++ cordova-plugin-wallet-suppression/src/android/WalletSuppression.java
@@ -1,4 +1,3 @@
-
 package org.apache.cordova.walletsuppression;
 
 import org.apache.cordova.CallbackContext;
@@ -9,29 +8,45 @@ import org.json.JSONException;
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
+import android.util.Log;
+
+import java.io.Console;
 
 
 public class WalletSuppression extends CordovaPlugin {
 
-  @Override
+  private static final String TAG = "WalletSuppression";
+
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
       if (action.equals("enableWallet")) {
-          NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(cordova.getActivity());
-          nfcAdapter.setNdefPushMessageCallback(null, cordova.getActivity());
-          callbackContext.success();
+          NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this.cordova.getActivity());
+          if (nfcAdapter != null) {
+            nfcAdapter.setNdefPushMessageCallback(null, this.cordova.getActivity());
+            callbackContext.success();
+            return true;
+          }
+          else{
+
+            Log.d(TAG, "No NFC Adapter found! ");
+          }
           return true;
       }
-     
+
       if (action.equals("disableWallet")) {
-        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(cordova.getActivity());
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this.cordova.getActivity());
+        if (nfcAdapter != null) {
         nfcAdapter.setNdefPushMessageCallback(new NfcAdapter.CreateNdefMessageCallback() {
             @Override
             public NdefMessage createNdefMessage(NfcEvent event) {
                 return null;
             }
-        }, cordova.getActivity());
+        }, this.cordova.getActivity());
         callbackContext.success();
-        return true;
+        return true;}
+        else{
+          Log.d(TAG, "No NFC Adapter found! ");
+
+        }
     }
     return false;
 }
